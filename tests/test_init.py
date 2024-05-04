25c8b91c0b753d509663670d3be7c3c8448fb91e
Daniel Tan <dtch1997@users.noreply.github.com>
2024-05-04 17:03:33 +0100

    chore: redefine project slug

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,6 @@
 def test_version():
     # Check that we can import the version number
-    from python_project_template import __version__
+    from pdm_python_project_template import __version__
 
     assert isinstance(__version__, str)
     assert __version__ != ""
